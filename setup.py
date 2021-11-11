973546bcb300ca687ccf0588a6013da58c6c8a83
Joe Cross <joe.mcross@gmail.com>
2021-11-10 18:05:59 -0800

    structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="protobloop",
+        version="0.1.0",
+        url="https://github.com/numberoverzero/protobloop",
+        package_dir={"": "src"},
+        packages = setuptools.find_packages(where="src"),
+        install_requires=["bloop~=3.0"],
+    )
